fcd2b35bc98f6a8d643c32c30771ee20bbcc39e6
Ahmed <waterydesert.developer@gmail.com>
2021-10-26 22:37:09 +0530

    remove web

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 void main() {
   runApp(const MyApp());
