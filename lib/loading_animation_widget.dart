436a8e3bc2dcacbca7314d73d37ae5c9d60e5ade
Ahmed <waterydesert.developer@gmail.com>
2021-10-26 22:44:26 +0530

    removed src

--- lib/loading_animation_widget.dart
+++ lib/loading_animation_widget.dart
@@ -2,15 +2,9 @@ library loading_animation_widget;
 
 import 'package:flutter/material.dart';
 
-import 'src/beat/beat.dart';
-
 class LoadingAnimationWidget {
   static Widget beat(
       {required Color color, required double size, int duration = 1000}) {
-    return Beat(
-      color: color,
-      size: size,
-      duration: duration,
-    );
+    return Container();
   }
 }
