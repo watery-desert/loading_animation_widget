031b946af361d9bd1fd6a666c69ee8c152ae8b61
Ahmed <waterydesert.developer@gmail.com>
2021-10-27 07:48:52 +0530

    added pubignore

--- lib/loading_animation_widget.dart
+++ lib/loading_animation_widget.dart
@@ -1,10 +1,15 @@
 library loading_animation_widget;
 
 import 'package:flutter/material.dart';
+import 'src/beat/beat.dart';
 
 class LoadingAnimationWidget {
   static Widget beat(
       {required Color color, required double size, int duration = 1000}) {
-    return Container();
+    return Beat(
+      color: color,
+      size: size,
+      duration: duration,
+    );
   }
 }
